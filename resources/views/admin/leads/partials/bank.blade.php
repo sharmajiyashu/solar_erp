f78d0d7e4858cfd96f48303ca83cd77b5efc1269
jangidkapilyashu <jangidkapilyashu@gmail.com>
2026-03-01 00:47:30 +0530

    update changes in this

--- resources/views/admin/leads/partials/bank.blade.php
+++ resources/views/admin/leads/partials/bank.blade.php
@@ -72,6 +72,49 @@
                         Delete
                     </button>
 
+                    <div class="modal fade" id="deleteDocModal{{ $doc->id }}">
+                        <div class="modal-dialog">
+
+                            <div class="modal-content">
+
+                                <div class="modal-header">
+                                    <h5>Delete Document</h5>
+                                    <button class="btn-close" data-bs-dismiss="modal"></button>
+                                </div>
+
+                                <div class="modal-body">
+                                    <h5>Are you sure you want to delete this document?</h5>
+
+                                    <p class="text-danger">
+                                        {{ $doc->document_type }}
+                                    </p>
+                                </div>
+
+                                <div class="modal-footer">
+
+                                    <form action="{{ route('admin.bank-documents.destroy', $doc->id) }}"
+                                        method="POST">
+
+                                        @csrf
+                                        @method('DELETE')
+
+                                        <button class="btn btn-danger">
+                                            Yes Delete
+                                        </button>
+
+                                    </form>
+
+                                    <button class="btn btn-secondary" data-bs-dismiss="modal">
+                                        Cancel
+                                    </button>
+
+                                </div>
+
+                            </div>
+
+                        </div>
+                    </div>
+
                 </td>
 
             </tr>
@@ -89,47 +132,7 @@
 </table>
 
 
-<div class="modal fade" id="deleteDocModal{{ $doc->id }}">
-    <div class="modal-dialog">
 
-        <div class="modal-content">
-
-            <div class="modal-header">
-                <h5>Delete Document</h5>
-                <button class="btn-close" data-bs-dismiss="modal"></button>
-            </div>
-
-            <div class="modal-body">
-                <h5>Are you sure you want to delete this document?</h5>
-
-                <p class="text-danger">
-                    {{ $doc->document_type }}
-                </p>
-            </div>
-
-            <div class="modal-footer">
-
-                <form action="{{ route('admin.bank-documents.destroy', $doc->id) }}" method="POST">
-
-                    @csrf
-                    @method('DELETE')
-
-                    <button class="btn btn-danger">
-                        Yes Delete
-                    </button>
-
-                </form>
-
-                <button class="btn btn-secondary" data-bs-dismiss="modal">
-                    Cancel
-                </button>
-
-            </div>
-
-        </div>
-
-    </div>
-</div>
 
 <!-- ================= Upload Modal ================= -->
 
@@ -143,7 +146,7 @@
 
             <div class="modal-content">
 
-                <div class="modal-header bg-primary text-white">
+                <div class="modal-header">
                     <h5>Upload Bank Document</h5>
                     <button class="btn-close" data-bs-dismiss="modal"></button>
                 </div>
